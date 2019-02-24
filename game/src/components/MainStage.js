92959601e73b01c1a0b5c62a62597b773ae5e6eb
MichaelBoegner <cmikeb@gmail.com>
2019-02-23 20:52:02 -0500

    Created react-app and added react-game-kit
    Created react-app and added react-game-kit. Added base stage looping as well. Attempting to start simple sprite creation with keyboard input for movement.

---
+++ game/src/components/MainStage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+export default class MainStage extends Component {
+    render() {
+        return (
+          <Loop>
+            <Stage>
+              // Game specific components go here
+            </Stage>
+          </Loop>
+        );
+      }
+}
